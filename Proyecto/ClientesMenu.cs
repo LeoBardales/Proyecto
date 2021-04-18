e2530ec210701d32707fc6ea3bc7961a3dc8e74a
LeoBardales <67931879+LeoBardales@users.noreply.github.com>
2021-04-18 11:24:44 -0600

    primer

--- Proyecto/ClientesMenu.cs
+++ Proyecto/ClientesMenu.cs
@@ -22,5 +22,11 @@ namespace Proyecto
             Cliente frm = new Cliente();
             frm.Show();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaldoCdia frm = new SaldoCdia();
+            frm.Show();
+        }
     }
 }
